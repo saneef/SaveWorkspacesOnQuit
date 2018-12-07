9a357c66c31708a5f96ed3538894474aabd54fd1
saneef <hello@saneef.com>
2018-12-08 01:04:24 +0530

    style: remove old comments

--- SaveWorkspacesOnQuit.py
+++ SaveWorkspacesOnQuit.py
@@ -2,17 +2,7 @@ import sublime
 import sublime_plugin
 
 '''
-	Save Workspace (Close Workspace) and Quit
-
-	First save this file as:
-	~/Library/Application Support/Sublime Text 3/Packages/User/save_workspace_and_quit.py
-
-	To trigger the command with 'Command/Ctrl q',
-	add the following to your Sublime Text > Preferences > Keybindings - User
-
-	[
-		{ "keys": ["super+q"], "command": "save_workspace_and_quit"}
-	]
+	Save Workspaces and Quit
 '''
 
 class SaveWorkspacesOnQuitCommand(sublime_plugin.WindowCommand):
